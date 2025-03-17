ab2c9fe530a235744e726501e7f3911660f25d2c
Bruno <bruno.nogueira@tistto.com.br>
2025-03-17 18:39:08 -0300

    listas de exemplos de aula

---
+++ lista.py
@@ -0,0 +1,27 @@
+# #lista simples
+lista = ['bruno', 'joao', 'maria']
+print(lista)
+print(lista[2]) #para mostra a 3 posição da lista
+print(type(lista))
+print('*'*30)
+
+#lista de tuplas
+tupla = ('arroz', True, 10, 1.50)
+print(tupla)
+print(tupla[0])
+print(type(tupla))
+
+print('*'*30)
+
+#lista de dicionario
+dicionario = {'nome': 'bruno', 'idade': 25, 'id':2, 'altura': 1.75}
+print(dicionario)
+print(dicionario['nome'])
+print(type(dicionario))
+
+print('*'*30)
+
+#lista de conjunto
+conjunto = {'bruno', True, 10, 1.50}
+print(conjunto)
+print(type(conjunto))
